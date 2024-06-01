be6899f5e70025e6a077951be9ab333048ee5dba
Abdulla Faraz <abdulla.faraz@gmail.com>
2024-06-01 10:09:43 +0530

    added app bar and viem

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -2,12 +2,16 @@ import Orderbook from './components/orderbook/orderbook.tsx';
 import PlaceOrderWidget from './components/place-order-widget/place-order-widget.tsx';
 import { Box, CssBaseline, Grid, ThemeProvider } from '@mui/material';
 import { theme } from './theme.ts';
+import ApplicationBar from "./components/app-bar/app-bar.tsx";
+import Web3ConnectButton from "./components/web3-connect/web3-connect-button.tsx";
 
 function App() {
   return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
         {/* The rest of your application */}
+        <ApplicationBar><Web3ConnectButton /></ApplicationBar>
+
           <Grid
             container
             spacing={0}

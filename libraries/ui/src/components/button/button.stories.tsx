f244972d76f693bfed1abbec94cd2f40fbf7e031
Abdulla Faraz <abdulla.faraz@gmail.com>
2024-07-01 22:03:09 +0530

    added formatting

--- libraries/ui/src/components/button/button.stories.tsx
+++ libraries/ui/src/components/button/button.stories.tsx
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from './button';
+import { Button } from "./button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
@@ -12,8 +12,8 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: {
     asChild: false,
-    variant: 'default',
-    size: 'default',
+    variant: "default",
+    size: "default",
     //label: 'Button',
   },
   render: () => <Button>Hello</Button>,
